5f865a544efa52825699430c2822b31158f94cc0
Christian <alvescastro00cca13@gmail.com>
2020-10-15 11:36:30 -0300

    Configuring and creating the Data Base

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,14 @@
 import express from 'express'
 
+import './database/connection'
+
 const app = express()
+
+app.use(express.json())
+
+app.get('/users', (request, response) => {
+    return response.json({message: "relou uordi "})
+})
+
 
 app.listen(3333)    
